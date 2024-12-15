b0336f10d4ac4ef639f25f427021d7ce4b083669
Nicklaus <nick.marietta@csu.fullerton.edu>
2024-12-15 01:41:56 -0800

    basic functions put in

---
+++ memory_allocator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "memory_allocator.h"
+#include "minheap.h"
+
--- memory_allocator.h
+++ memory_allocator.h
@@ -1,7 +1,14 @@
-#define MEMORY_ALLOCATOR_H
 #ifndef MEMORY_ALLOCATOR_H
+#define MEMORY_ALLOCATOR_H
 
 #include <stdlib.h>
 
+void free_node(void *ptr);
+
+char* calloc_node(void *ptr, size_t size);
+
+void* realloc_node(void *ptr, size_t size);
+
+char* malloc_node(size_t size);
 
 #endif
